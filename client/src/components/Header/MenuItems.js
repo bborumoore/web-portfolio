9fdcd08dd870fc0177ee2f1f26905ccb48bc9206
bborumoore <bborumoore@gmail.com>
2021-07-06 09:52:05 -0400

    Set up boilerplate code, started editing some components

---
+++ client/src/components/Header/MenuItems.js
@@ -0,0 +1,31 @@
+export const MenuItems = [
+    {
+        title: "Home",
+        url: '/', /*this is where you put the link*/
+        cName: 'nav-links'
+    },
+    {
+        title: "Social Media",
+        url: 'https://www.linkedin.com/in/brianbmoore/', 
+        target: "_blank",
+        cName: 'nav-links'
+    },
+    {
+        title: "GitHub Profile",
+        url: 'https://github.com/bborumoore', 
+        target: "_blank",
+        cName: 'nav-links'
+    },
+    {
+        title: "Projects",
+        url: '/Projects', 
+        target: "_blank",
+        cName: 'nav-links'
+    },
+    {
+        title: "Resume",
+        url: 'https://docs.google.com/document/d/14ZfXrSlGobIvjeXGFH1xaRrJxYCh4RshMVg8YPLABcw/edit', 
+        target: "_blank",
+        cName: 'nav-links'
+    },
+]
